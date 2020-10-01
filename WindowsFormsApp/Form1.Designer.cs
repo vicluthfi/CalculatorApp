71ceb649125797d8a7194ad432a5d41d94a5e6f4
vicluthfi <muhammad.alghifari@students.amikom.ac.id>
2020-10-01 22:47:51 +0700

    CalculatorApp by Luthfi

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbOperasi = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "";
+            this.button1.AccessibleName = "";
+            this.button1.Location = new System.Drawing.Point(140, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(76, 61);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(139, 20);
+            this.txtNilaiA.TabIndex = 1;
+            this.txtNilaiA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(76, 87);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(139, 20);
+            this.txtNilaiB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nilai A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nilai B";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Operasi";
+            // 
+            // cmbOperasi
+            // 
+            this.cmbOperasi.FormattingEnabled = true;
+            this.cmbOperasi.Items.AddRange(new object[] {
+            "Penambahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.cmbOperasi.Location = new System.Drawing.Point(76, 16);
+            this.cmbOperasi.Name = "cmbOperasi";
+            this.cmbOperasi.Size = new System.Drawing.Size(139, 21);
+            this.cmbOperasi.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hasil";
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.Location = new System.Drawing.Point(76, 129);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(139, 30);
+            this.lstHasil.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(243, 231);
+            this.Controls.Add(this.lstHasil);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbOperasi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbOperasi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstHasil;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        private int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        private int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text);
+            var b = Convert.ToInt32(txtNilaiB.Text);
+
+            lstHasil.Items.Clear();
+
+            switch (cmbOperasi.SelectedIndex)
+            {
+                case 0:
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Penambahan(a, b)));
+                    break;
+                case 1:
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Pengurangan(a, b)));
+                    break;
+                case 2:
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Perkalian(a, b)));
+                    break;
+                case 3:
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Pembagian(a, b)));
+                    break;
+            }
+        }
+    }
+}
